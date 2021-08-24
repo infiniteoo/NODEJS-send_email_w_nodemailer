f0095c3618c24a4f9caa27cbd6e8bfc109e97d34
Troy Dorman <troydorman@gmail.com>
2021-08-24 08:22:59 -0600

    integrated stylesheet for project

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.engine("handlebars", expshbs());
 app.set("view engine", "handlebars");
 
 // static folder
-app.use("public", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("contact");
